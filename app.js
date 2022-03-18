fc434e5c42b707afe0f4d3c1ad6927d8eb8f797d
Kevin Phan <90020140+KevinFPhan@users.noreply.github.com>
2022-03-17 22:40:17 -0700

    asd

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
+const logo = document.getElementById('DesqueLogo');
 const nav = document.querySelector('nav');
 const sectionOne = document.querySelector(".hero");
 const sectionTwo = document.querySelector(".contact")
+const slider = document.querySelector('.slider')
+const innerSlider = document.querySelector('.slider-inner')
 
 const sectionOneOptions = {
     rootMargin: "-600px 0px 0px 0px"
@@ -27,6 +30,61 @@ const sectionTwoObserver = new IntersectionObserver(function(entries, sectionTwo
     })
 }, sectionOneOptions)
 
+window.onscroll = function() {scrollFunction()};
 
 sectionOneObserver.observe(sectionOne);
 sectionTwoObserver.observe(sectionTwo);
+
+
+
+// Function
+function scrollFunction() {
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        nav.style.height = "80px";
+    } else {
+        nav.style.height = "128px"
+    }
+}
+
+let pressed = false;
+let startx;
+let x;
+
+slider.addEventListener('mousedown', (e) => {
+    pressed = true;
+    startx = e.offsetX - innerSlider.offsetLeft;
+    slider.style.cursor = 'grabbing'
+});
+
+slider.addEventListener('mouseenter', () => {
+    slider.style.cursor = 'grab'
+});
+
+slider.addEventListener('mouseup', () => {
+    slider.style.cursor = 'grab'
+});
+
+window.addEventListener('mouseup', () => {
+    pressed = false;
+})
+
+slider.addEventListener('mousemove', (e) => {
+    if(!pressed) return;
+    e.preventDefault();
+    console.log(MouseEvent);
+    x = e.offsetX
+
+    innerSlider.style.left = `${x - startx}px`;
+    checkBoundary();
+})
+
+function checkBoundary() {
+    let outer = slider.getBoundingClientRect();
+    let inner = innerSlider.getBoundingClientRect();
+
+    if(parseInt(innerSlider.style.left) > 0) {
+        innerSlider.style.left = '0px';
+    } else if (inner.right < outer.right) {
+        innerSlider.style.left = `-${inner.width - outer.width}px`
+    }
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,22 @@ module.exports = {
       'dark-bg': '#202124',
       'white': '#ffffff',
     },
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'client1-img' : "url('https://desque.sfo3.cdn.digitaloceanspaces.com/desque/191895310-300x225.jpg')",
+        'client1-logo' : "url('https://desque.sfo3.cdn.digitaloceanspaces.com/desque/Desque_Home_Clients_FastHammers-300x300.png')",
+        'client2-img' : "url('https://desque.sfo3.cdn.digitaloceanspaces.com/desque/annie-gray-382969-unsplash-300x200.jpg')",
+        'client2-logo' : "url('https://desque.sfo3.cdn.digitaloceanspaces.com/desque/Desque_Home_Clients_GlobalAirService2-300x300.png')",
+        'client3-img' : "url('https://desque.sfo3.cdn.digitaloceanspaces.com/desque/brian-babb-256298-unsplash-300x200.jpg')",
+        'client3-logo' : "url('https://desque.sfo3.cdn.digitaloceanspaces.com/desque/Desque_Home_Clients_IntegrateSun-300x300.png')",
+        'client4-img' : "url('https://desque.sfo3.cdn.digitaloceanspaces.com/desque/Desque_Portfolio_Substantia-Realty-300x169.jpg')",
+        'client4-logo' : "url('https://desque.sfo3.cdn.digitaloceanspaces.com/desque/Desque_Home_Clients_Pokeology-300x300.png')",
+        'client5-img' : "url('https://desque.sfo3.cdn.digitaloceanspaces.com/desque/Desque_Portfolio_Pokeology_Logo-300x300.jpg')",
+        'client5-logo' : "url('https://desque.sfo3.cdn.digitaloceanspaces.com/desque/gus-ruballo-128864-unsplash-300x200.jpg')",
+        'client6-img' : "url('https://desque.sfo3.cdn.digitaloceanspaces.com/desque/Desque_Portfolio_Pokeology_Logo-300x300.jpg')",
+        'client6-logo' : "url('https://desque.sfo3.cdn.digitaloceanspaces.com/desque/gus-ruballo-128864-unsplash-300x200.jpg')",
+      }
+    },
   },
   plugins: [],
 }
